4d5e2f920f24a8cc65a1a603637759d32e9d1d3e
Leszek Lesner <leszek.lesner@web.de>
2018-07-26 11:46:07 +0200

    Add script to allow copying zeronet to external usb drive or mmc

---
+++ usr/bin/zeronet-external.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+BLOCK_DEVICES=$(lsblk -d -n -oNAME,TYPE | grep disk | grep -v zram | awk '{print $1}')
+ORIG_LOCATION=/opt/zeronet/ZeroNet-master
+PLASMA_CONFIG_LOCATION=~/.config/plasma-org.kde.plasma.desktop-appletsrc
+
+function setLocation() {
+  LOC=$1
+  # Sed'ing is a bad idea and also for some reason does not work for me
+  #sed -i -e "s|^zeronetLocation=.*\$|zeronetLocation=$LOC|" $PLASMA_CONFIG_LOCATION
+  #sleep 2;
+  #sed -i -e "s|^zeronetLocation\[\$e\]=.*\$|zeronetLocation[\$e]=$LOC|" $PLASMA_CONFIG_LOCATION
+  # Linking instead
+  sudo mv $ORIG_LOCATION{,-backup}
+  sudo ln -s $LOC $ORIG_LOCATION
+}
+
+function cpZeronet() {
+  NEW_LOCATION=$1
+  echo "Copying Zeronet to new location: $NEW_LOCATION ..."
+  sudo cp -a $ORIG_LOCATION $NEW_LOCATION
+}
+
+function showPartitions() {
+  DISK=$1
+  PART=$(lsblk --list -n -oNAME,TYPE | grep part | grep -v zram | grep $DISK | awk '{print $1}')
+  echo $PART
+}
+
+function getFs() {
+  PART=$1
+  FS=$(lsblk --list -fs -n -oNAME,TYPE,FSTYPE | grep part | grep -v zram | grep $PART | awk '{print $3}')
+  echo $FS
+}
+
+function mountPartitions() {
+  PART=/dev/$1
+  MNTPNT=$2
+  sudo mount $1 $2 2>&1 > /dev/null
+}
+
+function add2fstab() {
+  PART=/dev/$1
+  MNTPNT=$2
+  FS=$(getFs $1)
+  sudo mkdir -p $MNTPNT
+  echo "$PART                                     $MNTPNT          $FS    defaults,nofail,x-systemd.device-timeout=4 0 0" | sudo tee --append /etc/fstab > /dev/null
+}
+
+function restartPlasma() {
+echo "Restarting Plasma..."
+kquitapp5 plasmashell
+sleep 2;
+plasmashell 2>&1 > /dev/null
+}
+
+function install() {
+ INPUT_PART=$(echo $1 | sed -e "s|^/dev/||")
+ add2fstab $INPUT_PART /mnt/zeronet-usb
+ mountPartitions $INPUT_PART /mnt/zeronet-usb
+ cpZeronet /mnt/zeronet-usb/
+ setLocation /mnt/zeronet-usb/ZeroNet-master
+ echo "Configuration finished. Please restart Zeronet."
+}
+
+function uninstall() {
+echo "Undo external zeronet"
+sudo rm /opt/zeronet/ZeroNet-master
+sudo mv /opt/zeronet/ZeroNet-master-backup /opt/zeronet/ZeroNet-master
+echo " " | sudo tee /etc/fstab 
+}
+
+function show_menu() {
+clear
+echo ""
+echo "This script will allow you to copy zeronet onto a usb device and use it from there."
+echo "Please choose which disk you want to use:"
+echo "Available devices:" 
+echo $BLOCK_DEVICES
+read -p "Enter device: " INPUT_DEVICE
+case "$BLOCK_DEVICES" in 
+  *$INPUT_DEVICE*)
+    echo "Which partition do you want to use?"
+    echo "Available partitions:"
+    PARTS=$(showPartitions $INPUT_DEVICE)
+    echo $PARTS
+    read -p "Enter partition: " INPUT_PART
+    case "$PARTS" in
+      *$INPUT_PART*)
+        add2fstab $INPUT_PART /mnt/zeronet-usb
+        mountPartitions $INPUT_PART /mnt/zeronet-usb
+        cpZeronet /mnt/zeronet-usb/
+        setLocation /mnt/zeronet-usb/ZeroNet-master
+        ;;
+      *) echo "Error. You can only enter one of the listed partitions" ;;
+    esac
+    ;;
+  *) echo "Error. You can only enter one of the listed devices" ;;
+esac
+}
+
+if [[ "$1" == "-u" ]]; then
+  uninstall
+elif [[ "$1" == "-i" ]] && [[ ! -z "$2" ]]; then
+  install $2
+else
+  show_menu
+fi
