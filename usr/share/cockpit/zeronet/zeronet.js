9d5d59b84b4839c397306eba7f20cf4df515a023
Kai Uwe Broulik <kde@privat.broulik.de>
2019-02-11 12:16:20 +0100

    Rewrite to use newly added convenience classes

--- usr/share/cockpit/zeronet/zeronet.js
+++ usr/share/cockpit/zeronet/zeronet.js
@@ -1,337 +1,227 @@
-var stat = $("#stat");
-var btn = $("#start");
-var openBtn = $("#open");
-var advBtn = $("#openSystemd");
-var isRunning = false;
 
-var SYSTEMD_BUSNAME = "org.freedesktop.systemd1"
+class Zeronet
+{
 
-var systemd_client = cockpit.dbus(SYSTEMD_BUSNAME, { superuser: "try" });
-var systemd_manager = systemd_client.proxy("org.freedesktop.systemd1.Manager",
-					   "/org/freedesktop/systemd1"); 
+    static _unitName = "zeronet.service";
 
-$("#statusSpinner").toggle(false)
+    _systemd = new Systemd();
+    _udisks = new UDisks();
 
-function checkStatus() {
-  //var proc2 = cockpit.spawn(["zeronet-startstop.sh", "-status"]);
-  //proc2.done(status_success);
-  //proc2.fail(status_fail);
-  $("#statusSpinner").toggle(true)
-  var unit;
-  systemd_manager.call("GetUnit", [ "zeronet.service" ]).
-  done(function (result) {
-    console.log(result[0]);
-    unit = result
-    systemd_client.call(unit[0], "org.freedesktop.DBus.Properties", "Get", [ "org.freedesktop.systemd1.Unit", "SubState" ]).
-    done(function (result) {
-      var state = result[0].v;
-      console.log("Zeronet Service subState: " + state);
-      if (state == "running") {
-	isRunning = true;
-	status_success();
-      }
-      else {
-	isRunning = false;
-	status_fail();
-      }
-    }).
-    fail(function (error) {
-      console.log(error)
+    _statusSpinner = $("#statusSpinner");
+    _statusLabel = $("#statusLabel");
+    _startStopBtn = $("#startStopBtn");
+    _openBtn = document.getElementById("openBtn");
+    _openSystemdBtn = document.getElementById("openSystemdBtn");
+
+    _zeronetUnit = null;
+
+    setupUi() {
+        $(this._statusSpinner).toggle(true);
+        this._startStopBtn.disabled = true;
+        $(this._openBtn).toggle(false);
+
+        this._openBtn.addEventListener("click", () => {
+            window.open("http://" + window.location.hostname);
+        });
+
+        this._openSystemdBtn.addEventListener("click", () => {
+            cockpit.jump('/system/services#/zeronet.service');
+        });
+
+        console.log(this._startStopBtn);
+        this._startStopBtn.click(() => {
+            if (!this._zeronetUnit) {
+                throw new TypeError("Cannot toggle start without unit");
+            }
+
+            switch (this._zeronetUnit.subState) {
+            case "running":
+                this._zeronetUnit.stop().then(() => {
+                    console.log("hallo")
+                }, (err) => {
+                    console.log("shit", err);
+                });
+                break;
+            case "failed":
+            case "dead":
+                this._zeronetUnit.start().then(() => {
+                    console.log("dead to life");
+                }, (err) => {
+                    console.log("shit", err);
+                });
+                break;
+            }
+        });
+
+    }
+
+    checkStatus() {
+        return new Promise((resolve, reject) => {
+
+            this._getUnit().then((unit) => {
+
+                this._updateStateUi(unit);
+
+            }, (err) => {
+                console.warn("Failed to get zeronet systemd unit", err);
+                this._statusLabel.innerText = "Error"; // not installed?
+            }).finally(() => {
+                this._statusSpinner.toggle(false);
+
+                resolve();
+            });
+        });
+    }
+
+    _updateStateUi(unit) {
+        console.log("UNIT CHANGE", unit.subState);
+
+        var text = "";
+        var color = "";
+        var busy = false;
+        var buttonText = "Start";
+
+        switch (unit.subState) {
+        case "start":
+            text = "Starting...";
+            color = "blue";
+            break;
+
+        case "stop-sigterm":
+        case "final-sigterm":
+            text = "Stopping...";
+            color = "orange";
+            break;
+
+        case "running":
+            text = "Running";
+            color = "green";
+            buttonText = "Stop";
+            break;
+
+        case "failed":
+        case "dead":
+            if (unit.canStart) {
+                color = "gray";
+                text = "Not running";
+            } else {
+                color = "red";
+                text = "Not installed";
+            }
+            break;
+
+        default:
+            console.log("Don't know what to do with state", unit.subState);
+            text = unit.subState;
+        }
+
+        $(this._openBtn).toggle(unit.subState === "running");
+        // TODO canStop when running
+        var enable = true;// unit.canStart && (unit.subState === "running" || unit.subState === "dead");
+        this._startStopBtn.prop("disabled", !enable);
+
+        this._statusLabel.css("color", color);
+        this._statusLabel.text(text);
+        this._statusSpinner.toggle(busy);
+        this._startStopBtn.text(buttonText);
+    }
+
+    _getUnit() {
+        return new Promise((resolve, reject) => {
+            if (this._zeronetUnit) {
+                return resolve(this._zeronetUnit);
+            }
+
+            this._systemd.getUnit(Zeronet._unitName).then((unit) => {
+                this._zeronetUnit = unit;
+
+                unit.onSubStateChanged(() => {
+                    this._updateStateUi(unit);
+                });
+
+                resolve(unit);
+            }, reject);
+        });
+    }
+
+}
+
+var zeronet = new Zeronet();
+zeronet.setupUi();
+zeronet.checkStatus();
+
+
+
+
+var udisks = new UDisks();
+
+udisks.removableDrives.then((drives) => {
+
+    var usbDevices = document.getElementById("usbDevices");
+    usbDevices.innerHtml = ""; // clear
+
+    drives.forEach((drive) => {
+        var headerDiv = document.createElement("div");
+        headerDiv.className = "panel panel-default";
+        headerDiv.innerHTML = `<div class='panel-heading'><b>USB Device: ${drive.vendor} ${drive.model}</b></div>`;
+        usbDevices.appendChild(headerDiv);
+
+        var partitionTable = document.createElement("table");
+        partitionTable.className = "table";
+
+        drive.partitions.forEach((partition) => {
+            var partitionTr = document.createElement("tr");
+
+            partitionTr.innerHTML = `
+                <td><img src='usbdev.png'></td>
+                <td>Device: ${partition.device}</td>
+                <td>File System: ${partition.filesystem}</td>
+                <td>Size: ${partition.size}</td>`;
+
+            partitionTable.appendChild(partitionTr);
+
+            // Created dynamically so we can keep a reference and update once the freeSpace promise finishes
+            var freeSpaceTd = document.createElement("td");
+            partitionTable.appendChild(freeSpaceTd);
+
+            // TODO mount
+
+            partition.mount().then((result) => {
+                console.log("MAUNT", result);
+            }, (err) => {
+                console.warn("Failed to mount", err);
+            });
+
+            partition.freeSpace.then((freeSpace) => {
+                freeSpaceTd.innerText = `Free: ${freeSpace}`;
+            }, (err) => {
+                console.warn("Failed to get free space for", partition.device, "on", partition.mountpoint, err);
+                freeSpaceTd.innerText = "Free: ???";
+            });
+
+            var buttonTd = document.createElement("td");
+
+            partition._button = document.createElement("button");
+            partition._button.className = "btn btn-default btn-secondary";
+
+            buttonTd.appendChild(partition._button);
+
+            partitionTable.appendChild(buttonTd);
+            // Created dynamically so we can add and remove buttons
+
+
+        });
+
+        usbDevices.appendChild(partitionTable);
+
+        console.log("USB DRIVE:", drive.vendor, drive.model);
+
+        drive.partitions.forEach((partition) => {
+            console.log("  Partition", partition.label, "on", partition.device);
+        });
+
+
     });
-  });
-  
-}
-
-/*function checkUsbDrive() {
-  var udisks_client = cockpit.dbus("org.freedesktop.UDisks2",
-                                           "/org/freedesktop/UDisks2");
-  var udisks_manager = udisks_client.proxy(udisks_client, "org.freedesktop.DBus.ObjectManager");
-  var managedObj;
-  udisks_manager.call("GetManagedObjects", []).
-  done(function (result) {
-      console.log("Result: " + result);
-      managedObj = result
-  }).fail(function (error) {
-     console.log(error)
-  });
-
-// Idea on how to identify the usb removable device
-//  for (k in managedObj) {
-//     console.log(k);
-//     var drive_info = k.call("get" , ['org.freedesktop.UDisks2.Drive']);
-//     console.log(drive_info);
-//     if (drive_info.get('ConnectionBus') == 'usb' && drive_info.get('Removable')) {
-//         if (drive_info['MediaRemovable']) {
-//          console.log("Device : " + k + " is a usb drive");
-//         }
-     //} 
-//  }
-}*/
-
-function checkUsbDrive() {
-  console.log("Checking for usb devices...");
-  var proc = cockpit.spawn(["zeronet-external.sh", "-j"]);
-  proc.done(checkUsb_success);
-  proc.fail(checkUsb_fail);
-}
-
-checkStatus();
-checkUsbDrive();
-
-$("#start").on("click", zeronet_toggle);
-$("#open").on("click", open_zeronet);
-$("#openSystemd").on("click", open_adv_settings);
-
-var usbDisks
-
-function checkUsb_success(data) {
-  var usbDisksOut = data
-  if (usbDisksOut != "") {
-     usbDisks = JSON.parse(usbDisksOut);
-     createUI_USB_devices();
-  }
-}
-
-function linkTo(dev) {
-  console.log("Called link to : " + dev.id)
-  if (isRunning) {
-   zeronet_stop()
-  }
-  var linkProc = cockpit.spawn(["zeronet-external.sh", "-a", dev.id]);
-  stat.css("color", "yellow");
-  stat.text("Activation running...");
-  btn.hide();
-  document.getElementById(dev.id).style.visibility="hidden";
-  linkProc.done(link_success);
-  linkProc.fail(link_fail);
-}
-
-function unlinkFrom(dev) {
-  console.log("Called link to : " + dev.id)
-  if (isRunning) {
-   zeronet_stop()
-  }
-  var unlinkProc = cockpit.spawn(["zeronet-external.sh", "-u"]);
-  stat.css("color", "yellow");
-  stat.text("Deactivation running...");
-  btn.hide();
-  document.getElementById(dev.id).style.visibility="hidden";
-  unlinkProc.done(unlink_success);
-  unlinkProc.fail(unlink_fail);
-}
-
-function migrateTo(dev) {
-  console.log("Called migrate to : " + dev.id)
-  if (isRunning) {
-   zeronet_stop()
-  }
-  var migrateProc = cockpit.spawn(["zeronet-external.sh", "-i", dev.id]);
-  stat.css("color", "yellow");
-  stat.text("Migration running...");
-  btn.hide();
-  document.getElementById(dev.id).style.visibility="hidden";
-  migrateProc.done(migrate_success);
-  migrateProc.fail(migrate_fail);
-}
-
-function migrate_success() {
-  stat.css("color", "green");
-  stat.text("Migration completed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function migrate_fail() {
-  stat.css("color", "red");
-  stat.text("Migration failed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function link_success() {
-  stat.css("color", "green");
-  stat.text("Activation completed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function link_fail() {
-  stat.css("color", "red");
-  stat.text("Activation failed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function unlink_success() {
-  stat.css("color", "green");
-  stat.text("Deactivation completed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function unlink_fail() {
-  stat.css("color", "red");
-  stat.text("Deactivation failed.");
-  btn.show();
-  checkUsbDrive();
-}
-
-function checkUsb_fail() {
-  console.log("no usb devices detected!")
-}
-
-// UI Creation functions TODO: Maybe put in external js file
-function createButton(type,id,name) {
-  return "<button class=\"btn btn-default btn-" + type + "\" id=\""+ id + "\">" + name + "</button>"
-}
-function createUSB_dev_header(text) {
-  return "<br/><div id=\"usbHeader " + text + "\" class=\"panel panel-default\"><div class=\"panel-heading\">\
-    <b>USB Device: " + text + "</b></span></div></div>"
-}
-function createMigrateButton(type,id,name) {
-  return "<button class=\"btn btn-default btn-" + type + "\" id=\""+ id + "\" onClick=\"migrateTo("+id+")\">" + name + "</button>"
-}
-function createLinkButton(type,id,name) {
-  return "<button class=\"btn btn-default btn-" + type + "\" id=\""+ id + "\" onClick=\"linkTo("+id+")\">" + name + "</button>"
-}
-function createUnlinkButton(type,id,name) {
-  return "<button class=\"btn btn-default btn-" + type + "\" id=\""+ id + "\" onClick=\"unlinkFrom("+id+")\">" + name + "</button>"
-}
-// END of UI Creation functions
-
-function createUI_USB_devices() {
-  // Create UI Elements
-  /*var usbHead = document.createElement("div", {
-                    className: "panel panel-default",
-                    id: "usbHeader" })
-  var usbHeading = document.createElement("div", {
-                    className: "panel-heading" })
-  usbHead.appendChild(usbHeading);
-  var rightSpan = document.createElement("span", {
-                    className: "pull-right" }) 
-  usbHeading.appendChild(rightSpan)
-  var usbText = document.createElement("span", null, "USB Devices");
-  rightSpan.appendChild(usbText);
-  var mainC = document.getElementById('main-container');
-  mainC.appendChild(usbHead);*/
-
-  console.log("Create UI called. usbDisks.disks.length: " + usbDisks.disks.length);
-  if (usbDisks.disks.length != 0) {
-
-     var mainC = document.getElementById('main-container');
-     if (document.contains(document.getElementById("usbDevs"))) {
-            document.getElementById("usbDevs").innerHTML = "";
-            document.getElementById("usbDevs").remove();
-     }
-     var deviceHtml = "<div id=usbDevs>";
-
-     for (var k=0; k<usbDisks.disks.length; k++) {
-        deviceHtml += createUSB_dev_header(usbDisks.disks[k].name + "  (" + usbDisks.disks[k].dev + ")")
-
-        deviceHtml += "<table class=\"table\">"
-        deviceHtml += "<tr><td><img src=\"usbdev.png\"></td>"
-        if ((usbDisks.disks[k].fs.indexOf("ext") != -1 || usbDisks.disks[k].fs.indexOf("xfs") != -1 ||
-        usbDisks.disks[k].fs.indexOf("btrfs") != -1 ||  usbDisks.disks[k].fs.indexOf("ntfs") != -1) &&
-        usbDisks.disks[k].stat.indexOf("internal") != -1) {
-           deviceHtml += "<td><p>Filesystem: " + usbDisks.disks[k].fs + "&nbsp;&nbsp;\
-                     </p></td><td><p>Size: " + usbDisks.disks[k].size + "</p></td><td>" + createMigrateButton("secondary",usbDisks.disks[k].name,"Migrate Zeronet here") + "</td></tr>"
-        }
-        else if ((usbDisks.disks[k].fs.indexOf("ext") != -1 || usbDisks.disks[k].fs.indexOf("xfs") != -1 ||
-        usbDisks.disks[k].fs.indexOf("btrfs") != -1 ||  usbDisks.disks[k].fs.indexOf("ntfs") != -1) &&
-        usbDisks.disks[k].stat.indexOf("not linked") != -1) {
-           deviceHtml += "<td><p>Filesystem: " + usbDisks.disks[k].fs + "&nbsp;&nbsp;\
-                     </p></td><td><p>Size: " + usbDisks.disks[k].size + "</p></td><td>" + createLinkButton("secondary",usbDisks.disks[k].name,"Use Zeronet from here") + "</td></tr>"
-        }
-        else if ((usbDisks.disks[k].fs.indexOf("ext") != -1 || usbDisks.disks[k].fs.indexOf("xfs") != -1 ||
-        usbDisks.disks[k].fs.indexOf("btrfs") != -1 ||  usbDisks.disks[k].fs.indexOf("ntfs") != -1) &&
-        usbDisks.disks[k].stat.indexOf("external") != -1) {
-           deviceHtml += "<td><p>Filesystem: " + usbDisks.disks[k].fs + "&nbsp;&nbsp;\
-                     </p></td><td><p>Size: " + usbDisks.disks[k].size + "</p></td><td>" + createUnlinkButton("secondary",usbDisks.disks[k].name,"Disable usage of Zeronet from here") + "</td></tr>"
-        }
-        else 
-            deviceHtml += "<td><p>Filesystem: " + usbDisks.disks[k].fs + "&nbsp;&nbsp;\
-                     </p></td><td><p>Size: " + usbDisks.disks[k].size + "</p></td>\
-                     <td><p>Use a linux compatible filesystem for usage with Zeronet.</p></td></tr>"
-            deviceHtml += "</table>"
-            console.debug("Found usb device: " + usbDisks.disks[k].name + " with filesystem: " + usbDisks.disks[k].fs);
-        }
-        deviceHtml += "</div>"
-        mainC.insertAdjacentHTML('beforeend', deviceHtml);
-  }
-}
-
-function zeronet_stop() {
-    systemd_manager.call("StopUnit", [ "zeronet.service", "replace" ]). 
-    done(function (result) {
-      console.log("Zeronet.service stop: " + result);
-      zeronet_success();
-    }).
-    fail(function (error) {
-      console.log(error);
-      zeronet_fail();
-      // Maybe some other error message that indicates that the service is not installed
-    });
-}
-
-function zeronet_start() {
-  stat.empty();
-  if (!isRunning) {
-    systemd_manager.call("StartUnit", [ "zeronet.service", "replace" ]). 
-    done(function (result) {
-      console.log("Zeronet.service start: " + result);
-      zeronet_success();
-    }).
-    fail(function (error) {
-      console.log(error);
-      zeronet_fail();
-      // Maybe some other error message that indicates that the service is not installed
-    });
-  }
-}
-
-function zeronet_toggle() {
-  if (!isRunning) {
-   zeronet_start();
-  }
-  else {
-   zeronet_stop();
-  }
-}
-
-
-function open_zeronet() {
-  var hostname = window.location.hostname
-  window.open("http://" + hostname);
-}
-
-function open_adv_settings() {
-  cockpit.jump('/system/services#/zeronet.service');
-}
-
-function zeronet_success() {
-  stat.css("color", "green");
-  stat.text("checking...");
-  checkStatus();
-}
-
-function zeronet_fail() {
-  stat.css("color", "red");
-  stat.text("not running");
-  checkStatus();
-}
-
-function status_success() {
-  $("#statusSpinner").toggle(false);
-  stat.css("color", "green");
-  stat.text("running");
-  btn.text("Stop");
-  openBtn.show();
-}
-
-function status_fail() {
-  $("#statusSpinner").toggle(false);
-  stat.css("color", "red");
-  stat.text("not running");
-  btn.text("Start");
-  openBtn.hide();
-}
+}, (err) => {
+    console.warn("ERR", err);
+});
